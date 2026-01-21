25e9eb3ea5d1ed5bfa7b29db06c55c26365eef23
SundownerSport <kilchisblue@aol.com>
2026-01-20 21:03:11 -0800

    make the window 2x wide and 1.5x tall

--- src/config.rs
+++ src/config.rs
@@ -67,10 +67,10 @@ pub const ASSET_EXTENSION: &str = ".7z";
 // ----------------------------------------------------------------------------
 
 /// Default window size (width, height)
-pub const WINDOW_SIZE: (f32, f32) = (475.0, 275.0);
+pub const WINDOW_SIZE: (f32, f32) = (950.0, 412.5);
 
 /// Minimum window size (width, height)
-pub const WINDOW_MIN_SIZE: (f32, f32) = (475.0, 275.0);
+pub const WINDOW_MIN_SIZE: (f32, f32) = (950.0, 412.5);
 
 // ----------------------------------------------------------------------------
 // ICON CONFIGURATION
